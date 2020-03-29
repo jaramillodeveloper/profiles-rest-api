5cf611c254fc20125f0b65b0a73d3f024138e630
Jorge Jaramillo <jaramillodeveloper@gmail.com>
2020-03-29 00:39:43 +0000

    update API view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from rest_framework import status
+from profiles_api import serializers
 
 class HelloApiView(APIView):
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Returns a list of APIView features"""
@@ -12,4 +14,14 @@ class HelloApiView(APIView):
             'Gives you the most control over your logic',
             'Is mapped manually to URLs',
         ]
-        return Response({'message': 'Hello!', 'an_apiview': features})
+        return Response({'message': 'Hello!', 'features': features})
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        validUserInput = serializer.is_valid()
+        if validUserInput:
+            name = serializer.validated_data.get('name')
+            message = f'hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
